bf2515d5a6655fa7359525e3ec4b29a435daea12
Ashley <ashleyshi006@gmail.com>
2022-12-09 16:41:38 -0500

    set up environment

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ const userRouter = require('./routes/userRoutes');
 const app = express();
 
 // Middlewares
-app.use(morgan('dev'));
+if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
+
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
--- server.js
+++ server.js
@@ -1,8 +1,9 @@
+const dotenv = require('dotenv');
+dotenv.config({ path: './config.env' });
+
 const app = require('./app');
 
-console.log(process.env);
-
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
