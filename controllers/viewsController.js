33db41e0446238f3fefc92c682101d1578c9c545
Ashley <ashleyshi006@gmail.com>
2023-01-03 21:27:00 -0500

    build tour overview page

---
+++ controllers/viewsController.js
@@ -0,0 +1,17 @@
+const Tour = require('../models/tourModel');
+const catchAsync = require('../utils/catchAsync');
+
+exports.getOverview = catchAsync(async (req, res, next) => {
+  const tours = await Tour.find();
+
+  res.status(200).render('overview', {
+    title: 'All Tours',
+    tours
+  });
+});
+
+exports.getTour = (req, res) => {
+  res.status(200).render('tour', {
+    title: 'The Forest Hiker Tour'
+  });
+};
