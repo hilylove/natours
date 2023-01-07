f94f061d5237275b302b194567a93973fe1b342f
Ashley <ashleyshi006@gmail.com>
2023-01-06 22:01:41 -0500

    add log out the user

--- public/js/index.js
+++ public/js/index.js
@@ -1,11 +1,12 @@
 /* eslint-disable*/
 import '@babel/polyfill';
 import { displayMap } from './mapbox';
-import { login } from './login';
+import { login, logout } from './login';
 
 // DOM elements
 const mapBox = document.getElementById('map');
 const loginFrom = document.querySelector('.form');
+const logOutBtn = document.querySelector('.nav__el--logout');
 
 // Delegation
 if (mapBox) {
@@ -21,3 +22,5 @@ if (loginFrom) {
     login(email, password);
   });
 }
+
+if (logOutBtn) logOutBtn.addEventListener('click', logout);
