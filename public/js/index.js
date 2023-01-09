8966a765440387258b064e3a24ffb18701dee98c
Ashley <ashleyshi006@gmail.com>
2023-01-08 21:50:16 -0500

    updating user data with api

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,13 @@
 import '@babel/polyfill';
 import { displayMap } from './mapbox';
 import { login, logout } from './login';
+import { updateData } from './updateSettings';
 
 // DOM elements
 const mapBox = document.getElementById('map');
-const loginFrom = document.querySelector('.form');
+const loginForm = document.querySelector('.form--login');
 const logOutBtn = document.querySelector('.nav__el--logout');
+const userDataForm = document.querySelector('.form-user-data');
 
 // Delegation
 if (mapBox) {
@@ -14,13 +16,20 @@ if (mapBox) {
   displayMap(locations);
 }
 
-if (loginFrom) {
-  loginFrom.addEventListener('submit', e => {
+if (loginForm)
+  loginForm.addEventListener('submit', e => {
     e.preventDefault();
     const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
     login(email, password);
   });
-}
 
 if (logOutBtn) logOutBtn.addEventListener('click', logout);
+
+if (userDataForm)
+  userDataForm.addEventListener('submit', e => {
+    e.preventDefault();
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    updateData(name, email);
+  });
